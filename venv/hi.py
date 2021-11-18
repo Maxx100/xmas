ec7583c17db8fcf9575025bfd563879c68a9ddd1
AmirHai <59731951+AmirHai@users.noreply.github.com>
2021-11-18 12:50:01 +0300

    base

--- venv/hi.py
+++ venv/hi.py
@@ -1,5 +1,9 @@
-'''
-план действий:
-прячем моник от камер
+a = [int(i) for i in input().split()]
 
-'''
+poluch = a[0]
+hod = a[1]
+
+if poluch % hod != 0:
+    print(poluch - poluch // hod)
+else:
+    print(poluch - poluch // hod + 1)
