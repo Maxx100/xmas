68401307bf7b0c87486dbdc9299c085eee21460f
rezon <rezonyou@gmail.com>
2021-11-20 18:12:48 +0300

    base

--- venv/xmas.py
+++ venv/xmas.py
@@ -1,29 +1,21 @@
-# A --------------------------
-# B --------------------------
-# C --------------------------
-n = int(input())
-
-a = [9, 8, 7, 6, 5, 4, 3, 2, 1]
-ans = []
-
-for i in a:
-    if n % i == 0:
-        ans.append(i)
-        n //= i
-if 8 in ans:
-    if 4 not in ans and 2 not in ans:
-        ans.remove(8)
-        ans.append(4)
-        ans.append(2)
-if 6 in ans:
-    if 3 not in ans and 2 not in ans:
-        ans.remove(6)
-        ans.append(3)
-        ans.append(2)
-ans.sort(reverse=True)
-if n == 1:
-    print("".join(list(map(str, ans))))
-else:
-    print(-1)
-# D --------------------------
-# E --------------------------
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    if sum(a) % 2 == 1:
+        print("NO")
+    else:
+        temp1 = 0
+        temp2 = 0
+        for i in a:
+            if i == 1:
+                temp1 += 1
+            else:
+                temp2 += 1
+        temp2 %= 2
+        if temp2 == 1:
+            if temp1 < 2:
+                print("NO")
+            else:
+                print("YES")
+        else:
+            print("YES")
