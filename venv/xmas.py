88f0a8bf316f13549a5c3cc4a988e8fbe13d5e71
rezon <rezonyou@gmail.com>
2021-11-20 22:13:10 +0300

    base

--- venv/xmas.py
+++ venv/xmas.py
@@ -1,21 +1,9 @@
-for _ in range(int(input())):
-    n = int(input())
-    a = list(map(int, input().split()))
-    if sum(a) % 2 == 1:
-        print("NO")
-    else:
-        temp1 = 0
-        temp2 = 0
-        for i in a:
-            if i == 1:
-                temp1 += 1
-            else:
-                temp2 += 1
-        temp2 %= 2
-        if temp2 == 1:
-            if temp1 < 2:
-                print("NO")
-            else:
-                print("YES")
-        else:
-            print("YES")
+a = int(input())
+b = int(input())
+ans = [a // 3, a % 3, b // 3]
+a %= 3
+b %= 3
+if a != b:
+    print(-1)
+else:
+    print(*ans)
