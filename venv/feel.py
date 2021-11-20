21076dfbb9af85dc579f443f6df27aedf6021c88
krami <1234567890.ru.com.net@mail.ru>
2021-11-20 22:12:09 +0300

    base

--- venv/feel.py
+++ venv/feel.py
@@ -1,1 +1,12 @@
-#здарова гей
+a = int(input())
+b = int(input())
+res = [0, 0, 0]
+res[0] = a // 3
+a = a % 3
+res[1] = b // 3
+b = b % 3
+if a != b:
+    print(-1)
+else:
+    res[2] = a
+    print(res[0], res[2], res[1])
